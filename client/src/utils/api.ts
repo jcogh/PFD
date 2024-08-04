cfff8f0e04e71f5856ab16516c00c7e54ef25c34
James Costello <james.m.costello@protonmail.com>
2024-08-04 16:58:44 -0700

    test

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'https://pfdapp-whrsa.ondigitalocean.app/api',
+	baseURL: 'https://pfdapp-whrsa.ondigitalocean.app',
 });
 
 api.interceptors.request.use(
