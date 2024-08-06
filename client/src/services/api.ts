e02b057a64ffd1852120666b10f0910ad4c2ad36
James Costello <james.m.costello@protonmail.com>
2024-08-06 15:47:54 -0700

    test

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://pfdapp-whrsa.ondigitalocean.app/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
